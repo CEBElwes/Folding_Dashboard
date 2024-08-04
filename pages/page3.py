fd721eb2ebf0496fb17974c7196189023420ca2e
Clementine Elwes <clementineelwes@clementines-mbp.home>
2024-08-04 23:38:26 +0100

    path to directory

--- pages/page3.py
+++ pages/page3.py
@@ -15,8 +15,8 @@ import seaborn as sns
 app = Dash(__name__)
 
 ## Read in data
-ddg_info = pd.read_csv("/mnt/azurefile/foldingdashboardNOTCH1/ddg_infoNOTCH1.csv")
-gene_pdbs = pd.read_csv("/mnt/azurefile/foldingdashboardNOTCH1/gene_pdbsNOTCH1.csv")
+ddg_info = pd.read_csv("/mnt/azurefile/foldingdashboard/ddg_infoNOTCH1.csv")
+gene_pdbs = pd.read_csv("/mnt/azurefile/foldingdashboard/gene_pdbsNOTCH1.csv")
 
 
 ### ----------------------
